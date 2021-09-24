f82a7a31ef7cd4f3c2a4caa4b5dba8e68ed3cf97
王旭 <1091156871@qq.com>
2021-09-24 14:49:46 +0800

    力扣704二分查找

---
+++ src/erfenchazhao704.java
@@ -0,0 +1,42 @@
+/*
+给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，写一个函数搜索nums中的 target，如果目标值存在返回下标，否则返回 -1。
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+
+提示：
+你可以假设 nums中的所有元素是不重复的。
+n将在[1, 10000]之间。
+nums的每个元素都将在[-9999, 9999]之间。
+
+ */
+public class erfenchazhao704 {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println(search(nums,target));
+    }
+    public static int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        int index=(left+right)/2;
+        while(left<=right){
+            if(nums[index]==target) return index;
+            if(nums[index]>target){
+                right=index-1;
+            }else{
+                left=index+1;
+            }
+            index = (right - left) / 2 + left;
+        }
+        return -1;
+    }
+}
