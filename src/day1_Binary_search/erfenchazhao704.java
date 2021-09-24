ad4542537aa6fb3901c9a75036f3e3c5bf4eaf9b
王旭 <1091156871@qq.com>
2021-09-24 18:39:07 +0800

    二分查找 力扣704

--- src/day1_Binary_search/erfenchazhao704.java
+++ src/day1_Binary_search/erfenchazhao704.java
@@ -24,6 +24,7 @@ public class erfenchazhao704 {
     public static void main(String[] args) {
         int[] nums = {-1, 0, 3, 5, 9, 12};
         int target = 66;
+
         System.out.println(search(nums,target));
     }
     public static int search(int[] nums, int target) {
